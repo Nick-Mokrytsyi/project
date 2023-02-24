331900c5f6acc30a8773f6604d4fab33f95f5988
nick_mokrytskyi <mykola.mokrytskyi@gmail.com>
2023-02-24 13:44:09 +0100

    create the function add_data_to_database

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.shortcuts import render
 from django.views.generic.list import ListView
 
 from .forms import ExpenseSearchForm
@@ -24,7 +27,25 @@ class ExpenseListView(ListView):
             summary_per_category=summary_per_category(queryset),
             **kwargs)
 
+
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
 
+
+def add_data_to_database(request):
+    """The function give as the opportunity to open/read and sava data from .json to our database(sqlite)"""
+    with open('fixtures.json', 'r') as file:
+        data = json.load(file)
+
+        for item in data:
+            if item['model'] == 'expenses.category':
+                category_fields = item['fields']
+                Category.objects.update_or_create(pk=item['pk'], defaults=category_fields)
+            elif item['model'] == 'expenses.expense':
+                expense_fields = item['fields']
+                category = Category.objects.get(pk=expense_fields['category'])
+                expense_fields['category'] = category
+                Expense.objects.update_or_create(pk=item['pk'], defaults=expense_fields)
+
+    return render(request, 'base.html')
