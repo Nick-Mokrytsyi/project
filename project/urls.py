1180a1d69c5877d5653b307cb7857214850bbce4
nick_mokrytskyi <mykola.mokrytskyi@gmail.com>
2023-02-24 13:38:37 +0100

    add a new route to create a data in database

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,14 @@
 from django.urls import include, path
 from django.views.generic.base import RedirectView
+from expenses.views import add_data_to_database
 
 urlpatterns = [
     path('',
          RedirectView.as_view(pattern_name='expenses:expense-list'),
          name='dashboard'),
+
     path('expenses/',
          include(('expenses.urls', 'expenses'), namespace='expenses')),
+
+    path('add_data/', add_data_to_database, name='add_data'),
 ]
