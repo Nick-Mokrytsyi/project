037b95866b98058e175c6a03fc5069e8b54453f1
nick_mokrytskyi <mykola.mokrytskyi@gmail.com>
2023-02-24 15:26:07 +0100

    add two new fields

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,11 +1,17 @@
 from django import forms
+from django.db.models import Q
+from django.shortcuts import render
+
 from .models import Expense
 
 
 class ExpenseSearchForm(forms.ModelForm):
+    from_date = forms.DateField(label='from', required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+    to_date = forms.DateField(label='to', required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+
     class Meta:
         model = Expense
-        fields = ('name',)
+        fields = ('name', 'from_date', 'to_date')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
